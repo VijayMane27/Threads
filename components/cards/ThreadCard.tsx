b036a152ad6af93375df915d9d9c368a1a716a7c
Vijay Mane <vijay.mane@salt-tech.com>
2024-07-10 18:33:13 +0530

    front page done

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,8 +1,10 @@
-import Link from "next/link";
 import Image from "next/image";
-import { formatDateString } from "@/lib/utils";
+import Link from "next/link";
 
-interface props {
+import { formatDateString } from "@/lib/utils";
+import DeleteThread from "../forms/DeleteThread";
+
+interface Props {
   id: string;
   currentUserId: string;
   parentId: string | null;
@@ -26,7 +28,7 @@ interface props {
   isComment?: boolean;
 }
 
-const ThreadCard = ({
+function ThreadCard({
   id,
   currentUserId,
   parentId,
@@ -36,10 +38,10 @@ const ThreadCard = ({
   createdAt,
   comments,
   isComment,
-}: props) => {
+}: Props) {
   return (
     <article
-      className={`flex w-full flex-col my-4 rounded-xl ${
+      className={`flex w-full mb-4 flex-col rounded-xl ${
         isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
       }`}
     >
@@ -49,19 +51,22 @@ const ThreadCard = ({
             <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
               <Image
                 src={author.image}
-                alt="Profile Image"
+                alt="user_community_image"
                 fill
                 className="cursor-pointer rounded-full"
               />
             </Link>
+
             <div className="thread-card_bar" />
           </div>
+
           <div className="flex w-full flex-col">
             <Link href={`/profile/${author.id}`} className="w-fit">
               <h4 className="cursor-pointer text-base-semibold text-light-1">
                 {author.name}
               </h4>
             </Link>
+
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
             <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
@@ -69,54 +74,86 @@ const ThreadCard = ({
                 <Image
                   src="/assets/heart-gray.svg"
                   alt="heart"
-                  className="cursor-pointer"
                   width={24}
                   height={24}
+                  className="cursor-pointer object-contain"
                 />
                 <Link href={`/thread/${id}`}>
                   <Image
                     src="/assets/reply.svg"
-                    alt="reply"
-                    className="cursor-pointer"
+                    alt="heart"
                     width={24}
                     height={24}
+                    className="cursor-pointer object-contain"
                   />
                 </Link>
                 <Image
                   src="/assets/repost.svg"
-                  alt="repost"
-                  className="cursor-pointer"
+                  alt="heart"
                   width={24}
                   height={24}
+                  className="cursor-pointer object-contain"
                 />
                 <Image
                   src="/assets/share.svg"
-                  alt="share"
-                  className="cursor-pointer"
+                  alt="heart"
                   width={24}
                   height={24}
+                  className="cursor-pointer object-contain"
                 />
               </div>
+
               {isComment && comments.length > 0 && (
                 <Link href={`/thread/${id}`}>
-                  <p className="mt-1 text-subtle-medium text-grey-1">
-                    {comments.length} repiles
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
                   </p>
                 </Link>
               )}
             </div>
           </div>
         </div>
+
+        <DeleteThread
+          threadId={JSON.stringify(id)}
+          currentUserId={currentUserId}
+          authorId={author.id}
+          parentId={parentId}
+          isComment={isComment}
+        />
       </div>
+
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+            </p>
+          </Link>
+        </div>
+      )}
+
       {!isComment && community && (
         <Link
-          className="mt-5 flex items-center"
           href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
         >
           <p className="text-subtle-medium text-gray-1">
-            {formatDateString(createdAt)} - {""}
-            {community.name} Community
+            {formatDateString(createdAt)}
+            {community && ` - ${community.name} Community`}
           </p>
+
           <Image
             src={community.image}
             alt={community.name}
@@ -128,6 +165,6 @@ const ThreadCard = ({
       )}
     </article>
   );
-};
+}
 
 export default ThreadCard;
